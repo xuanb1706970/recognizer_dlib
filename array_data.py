6ab3e2abc7d707e8b76a9fae4fd212f46daacc6e
xuanb1706970 <xuanb1706970@student.ctu.edu.vn>
2021-10-14 18:49:16 +0700

    udpate

--- array_data.py
+++ array_data.py
@@ -1,20 +1,38 @@
 from detector_faces import detector_face
-from create_data import read_csv, write_label
+from create_data import read_csv
 import cv2
 import os
 
 def array_train(data_path):
-    check = 0
-    data = read_csv('labels.csv')
     faces = []
     labels = []
-    write_label_csv = []
-
+    write_labels = []
     for i in os.listdir(data_path):
         path_fl = data_path + '/' + i
         label = int(i)
-        print(i)
-        write_label_csv.append(i)
+        write_labels.append(i)
+        for j in os.listdir(path_fl):
+            path_img = path_fl + '/' + j
+            image = detector_face(path_img)
+            if image is not None:
+                faces.append(image)
+                labels.append(label)
+                cv2.imshow('faces', image)
+                cv2.waitKey(1)
+    cv2.destroyAllWindows()
+
+    return faces, labels, write_labels
+
+def array_update(data_path):
+    check = 0
+    data = read_csv('labels.csv')
+    write_label = []
+    faces = []
+    labels = []
+    for i in os.listdir(data_path):
+        path_fl = data_path + '/' + i
+        label = int(i)
+        write_label.append(i)
         for emp in data:
             if int(emp) == int(i):
                 check = 1
@@ -24,14 +42,19 @@ def array_train(data_path):
         if check == 2:
             for j in os.listdir(path_fl):
                 path_img = path_fl + '/' + j
-                img = detector_face(path_img)
-                if img is not None:
-                    faces.append(img)
+                image = detector_face(path_img)
+                if image is not None:
+                    faces.append(image)
                     labels.append(label)
+                    cv2.imshow('faces', image)
+                    cv2.waitKey(1)
+            cv2.destroyAllWindows()
 
-    cv2.waitKey(1)
-    cv2.destroyAllWindows()
+            return faces, labels, write_label
 
-    return faces, labels, write_label_csv
+
+
+
+
 
 
